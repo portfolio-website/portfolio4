999b76a74a0dc98a6b910839538678e5718ab352
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-07-23 12:33:55 +0700

    First Commit

---
+++ mail/contact_me.php
@@ -0,0 +1,57 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once "vendor/autoload.php";
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+$mail = new PHPMailer(true);
+
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "pratamyogiyogi@gmail.com";                 
+$mail->Password = "kekuatan1";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to
+$mail->Port = 587;                                   
+
+$mail->From = "$email_address@gmail.com";
+$mail->FromName = "Full Name";
+
+$mail->addAddress("email_address", "Recepient Name");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
+return true;				
+?>
